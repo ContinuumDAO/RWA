7399a41324e0147fb8e030e482678c4c95cf24fe
Patrick Curé <paddy.cure@gmail.com>
2025-07-06 17:46:26 +0100

    Add proxies for protocol contracts

---
+++ src/managers/FeeManagerProxy.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity ^0.8.19;
+
+import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967Proxy.sol";
+
+contract FeeManagerProxy is ERC1967Proxy {
+    constructor (address implementation, bytes memory _data) ERC1967Proxy(implementation, _data) {}
+}
