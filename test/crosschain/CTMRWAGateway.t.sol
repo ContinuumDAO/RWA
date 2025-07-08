63b65a38b5fdc9f7d027a8dfa8b698960f156ebd
Selqui <continuumdao@proton.me>
2025-07-08 19:40:57 +0100

    Gateway test added

--- test/crosschain/CTMRWAGateway.t.sol
+++ test/crosschain/CTMRWAGateway.t.sol
@@ -35,34 +35,34 @@ contract TestGateway is Helpers {
         assertTrue(stringsEqual(gatewayStr, _toLower("ethereumGateway")));
     }
 
-    // function test_CTMRWAGateway_rwaXChain() public view {
-    //     uint256 nChains = gateway.getRWAXCount(RWA_TYPE, VERSION);
-    //     assertEq(nChains, 2);
+    function test_CTMRWAGateway_rwaXChain() public view {
+        uint256 nChains = gateway.getRWAXCount(RWA_TYPE, VERSION);
+        assertEq(nChains, 2);
 
-    //     nChains = gateway.getRWAXCount(RWA_TYPE + 1, VERSION);
-    //     assertEq(nChains, 0);   // There are no chains for rwaType == 2
+        nChains = gateway.getRWAXCount(RWA_TYPE + 1, VERSION);
+        assertEq(nChains, 0);   // There are no chains for rwaType == 2
 
-    //     string memory rwaxChainStr;
-    //     string memory chainIdStr;
-    //     bool ok;
-    //     // Check the local chain rwa1X, added automatically in initialize
-    //     (ok, rwaxChainStr) = gateway.getAttachedRWAX(RWA_TYPE, VERSION, cIdStr);
-    //     assertTrue(ok);
-    //     console.log(chainIdStr);
-    //     console.log(address(rwa1X).toHexString());
-    //     assertTrue(stringsEqual(rwaxChainStr, _toLower(address(rwa1X).toHexString())));
+        string memory rwaxChainStr;
+        string memory chainIdStr;
+        bool ok;
+        // Check the local chain rwa1X, added automatically in initialize
+        (ok, rwaxChainStr) = gateway.getAttachedRWAX(RWA_TYPE, VERSION, cIdStr);
+        assertTrue(ok);
+        console.log(chainIdStr);
+        console.log(address(rwa1X).toHexString());
+        assertTrue(stringsEqual(rwaxChainStr, _toLower(address(rwa1X).toHexString())));
 
-    //     // Check the rwaxChainStr at position 1
+        // Check the rwaxChainStr at position 1
        
-    //     uint256 pos = 1;
-    //     (chainIdStr, rwaxChainStr) = gateway.getAttachedRWAX(RWA_TYPE, VERSION, pos);
-    //     assertTrue(stringsEqual(chainIdStr, _toLower("NEARChainId")));
+        uint256 pos = 1;
+        (chainIdStr, rwaxChainStr) = gateway.getAttachedRWAX(RWA_TYPE, VERSION, pos);
+        assertTrue(stringsEqual(chainIdStr, _toLower("NEARChainId")));
 
-    //     chainIdStr = "NEARChainId";
-    //     assertTrue(stringsEqual(
-    //         rwaxChainStr,
-    //         _toLower("thisIsSomeRandomAddressOnNEARForTestingWallet123456789.test.near")
-    //     ));
-    // }
+        chainIdStr = "NEARChainId";
+        assertTrue(stringsEqual(
+            rwaxChainStr,
+            _toLower("thisIsSomeRandomAddressOnNEARForTestingWallet123456789.test.near")
+        ));
+    }
 
 }
