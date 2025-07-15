f0e26974eebcdc33d038863af57e8c04348c925b
Patrick Curé <paddy.cure@gmail.com>
2025-07-15 01:26:09 +0100

    Merge differing CTMRWATokenFactory interfaces

--- src/deployment/ICTMRWA1TokenFactory.sol
+++ src/deployment/ICTMRWA1TokenFactory.sol
@@ -7,5 +7,20 @@ import {Address} from "../CTMRWAUtils.sol";
 interface ICTMRWA1TokenFactory {
     error CTMRWA1TokenFactory_Unauthorized(Address);
 
-    function deploy(bytes memory _deployData) external returns (address);
+    function deploy(bytes memory deployData) external returns (address);
+
+    // TODO: do we implement these functions?
+    // function deployDividend(uint256 ID, address tokenAddr, uint256 rwaType, uint256 version, address ctmRwaMap)
+    //     external
+    //     returns (address);
+
+    // function deployStorage(uint256 ID, address tokenAddr, uint256 rwaType, uint256 version, address ctmRwaMap)
+    //     external
+    //     returns (address);
+
+    // function deploySentry(uint256 ID, address tokenAddr, uint256 rwaType, uint256 version, address ctmRwaMap)
+    //     external
+    //     returns (address);
+
+    // function setCtmRwaDeployer(address deployer) external;
 }
